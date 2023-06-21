5eb6077999dc40c6f13feb2701e2ca5524a88f40
Vera Samohina <samohinavera44@gmail.com>
2023-06-21 13:18:03 +0300

    add description and field in class Node and Stack in stack .py

--- src/stack.py
+++ src/stack.py
@@ -1,13 +1,15 @@
 class Node:
     """Класс для узла стека"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
+        :param next_node: узел, созданный до текущего. Когда создается первый экземпляр класса, то равен None
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,7 +17,9 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.stack = []
+        # Указатель вершины стека. Для пустого стека - None
+        self.top = None
 
     def push(self, data):
         """
@@ -23,7 +27,13 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        self.data = data
+        node = Node(data)
+        self.stack.append(self.data)
+       # После добавления элемента в стек, меняем указатель вершины стека и предыдущего узла
+        node.next_node = self.top
+        self.top = node
+
 
     def pop(self):
         """
