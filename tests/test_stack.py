92c8a095891ecf157c5007d4fc581feb8162aa1f
Vera Samohina <samohinavera44@gmail.com>
2023-06-24 14:05:50 +0300

    add tests for method pup()

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,22 +1,32 @@
 import unittest
-from src import stack
+from src.stack import Stack, Node
 
 class TestNode(unittest.TestCase):
     def test_next_node(self):
-        one = stack.Node(1)
-        two = stack.Node(2, 1)
-        three = stack.Node(3, 2)
+        one = Node(1)
+        two = Node(2, 1)
+        three = Node(3, 2)
         self.assertEqual(one.next_node, None)
         self.assertEqual(two.next_node, 1)
         self.assertEqual(three.next_node, 2)
 
-class TestStake(unittest.TestCase):
-    def test_stake(self):
-        stack1 = stack.Stack()
+class TestStack(unittest.TestCase):
+    def test_stack(self):
+        stack1 = Stack()
         with self.assertRaises(AttributeError):
             stack1.top.data, None
         stack1.push('ten')
         self.assertEqual(stack1.top.data, 'ten')
+
+    def test_pop(self):
+        stack2 = Stack()
+        stack2.push('one')
+        stack2.push('two')
+        self.assertEqual(stack2.pop(), 'two')
+        self.assertEqual(stack2.top.data, 'one')
+        stack2.pop()
+        self.assertEqual(stack2.top, None)
+        self.assertEqual(stack2.pop(), None)
 
 if __name__ == '__main__':
     unittest.main()
