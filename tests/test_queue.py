b2dca19e369d26a243ede08c56ba3aee8d91e6fe
Vera Samohina <samohinavera44@gmail.com>
2023-07-04 15:45:15 +0300

    add test for method dequeue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -7,20 +7,24 @@ class TestQueue(unittest.TestCase):
         first = Node(1)
         second = Node(2, 1)
         third = Node(3, 2)
-        self.assertEqual(first.next_node, None)
+        self.assertIsNone(first.next_node)
         self.assertEqual(second.next_node, 1)
         self.assertEqual(third.next_node, 2)
 
     def test_queue(self):
         queue1 = Queue()
-        self.assertEqual(queue1.head, None)
-        self.assertEqual(queue1.tail, None)
+        self.assertIsNone(queue1.head)
+        self.assertIsNone(queue1.tail)
         queue1.enqueue('first')
         queue1.enqueue('second')
         self.assertEqual(queue1.head.data, 'first')
         self.assertEqual(queue1.tail.data, 'second')
         self.assertEqual(queue1.head.next_node.data, 'second')
         self.assertEqual(str(queue1), 'first\nsecond')
+        self.assertEqual(queue1.dequeue(), 'first')
+        self.assertEqual(queue1.head.data, 'second')
+        self.assertEqual(queue1.dequeue(), 'second')
+        self.assertIsNone(queue1.dequeue())
 
 
 if __name__ == '__main__':
