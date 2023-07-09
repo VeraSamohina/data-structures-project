73dc7764f9ca541b36730ee209750492af147228
Vera Samohina <samohinavera44@gmail.com>
2023-07-09 16:04:40 +0300

    add class LinkedList

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,34 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        if self.head is not None:
+            node = Node(data, self.head)
+            self.head = node
+        elif self.head is None:
+            node = Node(data)
+            self.head = self.tail = node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        if self.head is not None:
+            node = Node(data)
+            self.tail.next_node = node
+            self.tail = node
+        elif self.head is None:
+            node = Node(data)
+            self.tail = self.head = node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -22,8 +38,8 @@ class LinkedList:
 
         ll_string = ''
         while node:
-            ll_string += f' {str(node.data)} ->'
+            ll_string += f'{str(node.data)} -> '
             node = node.next_node
 
-        ll_string += ' None'
+        ll_string += 'None'
         return ll_string
